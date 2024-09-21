ac3251eda90cd02d5a80d99c49f300976dc2dd82
S Mahmudul Hasan <mahmudulhasan947@gmail.com>
2024-09-21 01:57:47 -0500

    Inital setup

---
+++ project-manager-backend/src/main/java/com/numan947/pmbackend/ProjectManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.numan947.pmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagerBackendApplication.class, args);
+	}
+
+}
---
+++ project-manager-backend/src/test/java/com/numan947/pmbackend/ProjectManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numan947.pmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
